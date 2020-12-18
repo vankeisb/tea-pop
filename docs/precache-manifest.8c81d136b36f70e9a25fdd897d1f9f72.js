6081a402ddc7b7074616cd415a969943f4d7b750
Rémi Van Keisbelck <remi@rvkb.com>
2020-12-18 21:41:23 +0100

    docs

---
+++ docs/precache-manifest.8c81d136b36f70e9a25fdd897d1f9f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd3ec44ce1a11809ba9ae2993c3924b",
+    "url": "https://vankeisb.github.io/tea-pop/index.html"
+  },
+  {
+    "revision": "d8fcbb17fec8d2901291",
+    "url": "https://vankeisb.github.io/tea-pop/static/css/main.6a7926cd.chunk.css"
+  },
+  {
+    "revision": "34808b3890f58f292d2b",
+    "url": "https://vankeisb.github.io/tea-pop/static/js/2.f426c249.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://vankeisb.github.io/tea-pop/static/js/2.f426c249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8fcbb17fec8d2901291",
+    "url": "https://vankeisb.github.io/tea-pop/static/js/main.57cf1444.chunk.js"
+  },
+  {
+    "revision": "4fb6317dfac770caeb66",
+    "url": "https://vankeisb.github.io/tea-pop/static/js/runtime-main.c9d1acc6.js"
+  }
+]);
