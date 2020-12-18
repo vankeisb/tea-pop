e5ad01364e1567ba1104d78c137dfbec565993cb
Rémi Van Keisbelck <remi@rvkb.com>
2020-12-18 21:10:13 +0100

    doc

---
+++ doc/precache-manifest.70b99464d67db4cd2c481efa4c47c9b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d939b76019fd3ffee6b1f9e65bf42a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0bfeacaa4ad4ffe3b5d",
+    "url": "/static/css/main.6a7926cd.chunk.css"
+  },
+  {
+    "revision": "34808b3890f58f292d2b",
+    "url": "/static/js/2.f426c249.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f426c249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bfeacaa4ad4ffe3b5d",
+    "url": "/static/js/main.0ff57bee.chunk.js"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
