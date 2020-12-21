67ad64fd952a5cef7f517dfb51b641de728e4d46
Rémi Van Keisbelck <remi@rvkb.com>
2020-12-21 11:56:07 +0100

    doc

--- demo/src/Demo.tsx
+++ demo/src/Demo.tsx
@@ -55,6 +55,7 @@ function onKeyDown(key: string): Msg {
  * We keep our menus as constants. Those are, like everything, immutable.
  * They will serve as prototypes for when we open menus in reaction to clicks or
  * keystrokes...
+ * We use strings for our data types, but it could hold a richer data type.
  */
 
 const mySubMenu2: Menu<string> = menu([
@@ -78,15 +79,22 @@ const myMenu: Menu<string> = menu([
   item("I am a bit longer")
 ]);
 
+/**
+ * We use the default item renderer with our item data type (string).
+ */
+
 const MyRenderer = defaultItemRenderer((s: string) => <span>{s}</span>);
 
+/*
+ * init, view, update, subs...
+ */
+
 export function init(): [Model, Cmd<Msg>] {
-  const model: Model = {
+  return noCmd({
     menuModel: nothing,
     lastClicked: nothing,
     mousePos: Pos.origin
-  }
-  return noCmd(model);
+  });
 }
 
 export function view(dispatch: Dispatcher<Msg>, model: Model) {
@@ -108,6 +116,7 @@ export function view(dispatch: Dispatcher<Msg>, model: Model) {
               )
           }
         </div>
+        {/* include the Menu if it's open */}
         {model.menuModel
             .map(menuModel =>
                 <ViewMenu model={menuModel} dispatch={map(dispatch, menuMsg)} renderer={MyRenderer}/>
@@ -118,16 +127,10 @@ export function view(dispatch: Dispatcher<Msg>, model: Model) {
   )
 }
 
-function updateMenu(model: Model, mac: [MenuModel, Cmd<MenuMsg>]): [Model, Cmd<Msg>] {
-  return Tuple.fromNative(mac)
-      .mapFirst(mm => ({...model, menuModel: just(mm)}))
-      .mapSecond(mc => mc.map(menuMsg))
-      .toNative();
-}
-
 export function update(msg: Msg, model: Model): [Model, Cmd<Msg>] {
   switch (msg.tag) {
     case "menu-msg": {
+      // got child msg, update our child...
       if (model.menuModel.type === 'Nothing') {
         return noCmd(model);
       }
@@ -138,6 +141,7 @@ export function update(msg: Msg, model: Model): [Model, Cmd<Msg>] {
       };
       const cmd: Cmd<Msg> = mco[1].map(menuMsg);
       const mac: [Model, Cmd<Msg>] = [newModel, cmd];
+      // and handle its out msg if needed
       const outMsg: Maybe<TM.OutMsg<string>> = mco[2];
       return outMsg
           // eslint-disable-next-line array-callback-return
@@ -152,12 +156,15 @@ export function update(msg: Msg, model: Model): [Model, Cmd<Msg>] {
           .withDefault(mac);
     }
     case "mouse-move": {
+      // keep track of the mouse pos (needed for opening with keyboard)
       return noCmd({...model, mousePos: msg.pos })
     }
     case "mouse-down": {
+      // open menu on right click
       if (msg.button === 2) {
         return updateMenu(model, TM.open(myMenu, model.mousePos));
       }
+      // or close
       return noCmd({
         ...model,
         menuModel: nothing,
@@ -165,6 +172,7 @@ export function update(msg: Msg, model: Model): [Model, Cmd<Msg>] {
       });
     }
     case "key-down": {
+      // open menu on context menu key
       if (msg.key === 'ContextMenu') {
         return updateMenu(model, TM.open(myMenu, model.mousePos));
       }
@@ -173,17 +181,31 @@ export function update(msg: Msg, model: Model): [Model, Cmd<Msg>] {
   }
 }
 
-function closeMenu(model: Model, lastClicked: Maybe<string> = nothing): [Model, Cmd<Msg>] {
-  return noCmd({...model, menuModel: nothing, lastClicked })
-}
-
 const documentEvents = new DocumentEvents();
 
 export function subscriptions(model: Model): Sub<Msg> {
+  // the menu's subs
   const menuSub: Sub<Msg> = model.menuModel
       .map(mm => TM.subscriptions(mm).map(menuMsg))
       .withDefaultSupply(() => Sub.none());
+  // mouse & key subs
   const mouseMove: Sub<Msg> = documentEvents.on('mousemove', e => onMouseMove(TM.pos(e.pageX, e.pageY)));
   const keyDown: Sub<Msg> = documentEvents.on('keydown', e => onKeyDown(e.key));
   return Sub.batch([menuSub, mouseMove, keyDown]);
 }
+
+/*
+ * Helper functions
+ */
+
+function updateMenu(model: Model, mac: [MenuModel, Cmd<MenuMsg>]): [Model, Cmd<Msg>] {
+  return Tuple.fromNative(mac)
+      .mapFirst(mm => ({...model, menuModel: just(mm)}))
+      .mapSecond(mc => mc.map(menuMsg))
+      .toNative();
+}
+
+function closeMenu(model: Model, lastClicked: Maybe<string> = nothing): [Model, Cmd<Msg>] {
+  return noCmd({...model, menuModel: nothing, lastClicked })
+}
+
