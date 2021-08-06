b590111ce5907561886fcd13373fa4a5930139e5
Rémi Van Keisbelck <remi@rvkb.com>
2021-08-06 11:16:05 +0200

    release script

---
+++ release.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+ # exit if VERSION var not set
+[[ -z "$VERSION" ]] && { echo "VERSION not set" ; exit 1; }
+
+echo "Releasing VERSION=$VERSION"
+
+# make sure develop & main are up to date
+git co main 
+git pull
+git co develop
+git pull
+
+echo -n "Is it ok (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo "Creating release branch"
+else
+    echo "Aborting"
+    exit
+fi
+
+# start release branch
+git flow release start $VERSION
+
+# bump versions
+node ./bump-version.js
+
+echo "Versions bumped."
+git st
+
+echo -n "Commit and finish release (y/n)? "
+read answer2
+if [ "$answer2" != "${answer2#[Yy]}" ] ;then
+    echo "Finishing release"
+else
+    echo "Aborting"
+    exit
+fi
+
+git ci -a -m "bump versions $VERSION"
+git flow release finish $VERSION
+
+echo "Pushing"
+#git push --follow-tags
+#git push origin main
+#git push origin develop
+
+
+
+
+
+
+
