411cb76937e451c75b325229cb92bbc5ac5d4260
Rémi Van Keisbelck <remi@rvkb.com>
2023-12-07 12:36:20 +0100

    sandbox

--- components/sandbox/src/index.ts
+++ components/sandbox/src/index.ts
@@ -2,7 +2,7 @@ import { Dim, box, pos } from 'tea-pop-core';
 import { Menu, MenuItem } from 'tea-pop-menu';
 import * as TPM from 'tea-pop-menu';
 
-import './style.css';
+import './style.scss';
 import { ItemSeparator } from './my-items/ItemSeparator';
 import { MyItemContent } from './my-items/MyItemContent';
 
@@ -13,15 +13,30 @@ TPM.defineCustomElements();
 customElements.define('my-item-separator', ItemSeparator);
 customElements.define('my-item-content', MyItemContent);
 
-// declarative
+// setup sandbox UI
+
+export type Flavor = 'programmatic' | 'declarative';
+
+const rDecl = document.getElementById('r-decl') as HTMLInputElement;
+const rProg = document.getElementById('r-prog') as HTMLInputElement;
+
+function inputFlavor(): Flavor {
+  if (rProg.checked) {
+    return 'programmatic';
+  }
+  return 'declarative';
+}
+
+let flavor = inputFlavor();
+
+function updateFlavor() {
+  flavor = inputFlavor();
+}
+
+rDecl.addEventListener('change', updateFlavor);
+rProg.addEventListener('change', updateFlavor);
 
 const myMenu: Menu = document.getElementById('my-menu') as Menu;
-
-document.getElementById('declarative').addEventListener('contextmenu', (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  myMenu.open(box(pos(e.clientX, e.clientY), Dim.zero));
-});
 
 myMenu.addMenuListener('open', (e) => {
   console.log('[sandbox] declarative open', e.menu);
@@ -36,7 +51,45 @@ myMenu.addMenuListener('close', (e) => {
   console.log('[sandbox] declarative closed', e.menu);
 });
 
-// programmatic
+function getMenu(): Menu {
+  switch (flavor) {
+    case 'declarative': {
+      return myMenu;
+    }
+    case 'programmatic': {
+      const menu = programmaticMenu();
+      menu.addMenuListener('open', (e) => {
+        console.log('[sandbox] programmatic open', e.menu);
+      });
+      menu.addMenuListener('close', (e) => {
+        console.log('[sanbox] programmatic closed');
+        e.menu.remove();
+      });
+      menu.addMenuListener('itemSelected', (e) => {
+        console.log(
+          '[sanbox] programmatic selected',
+          e.item,
+          'in menu',
+          e.menu,
+        );
+        menu.close();
+      });
+      document.body.appendChild(menu);
+      return menu;
+    }
+  }
+}
+
+document.addEventListener(
+  'contextmenu',
+  (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const menu = getMenu();
+    menu.open(box(pos(e.clientX, e.clientY), Dim.zero));
+  },
+  { capture: true },
+);
 
 function programmaticMenu(): Menu {
   const menu = new Menu();
@@ -49,18 +102,12 @@ function programmaticMenu(): Menu {
   return menu;
 }
 
-document.getElementById('programmatic').addEventListener('contextmenu', (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  const menu = programmaticMenu();
-  menu.addMenuListener('close', (e) => {
-    console.log('[sanbox] programmatic closed');
-    e.menu.remove();
-  });
-  document.body.appendChild(menu);
-  menu.open(box(pos(e.clientX, e.clientY), Dim.zero));
-  menu.addMenuListener('itemSelected', (e) => {
-    console.log('[sanbox] programmatic selected', e.item, 'in menu', e.menu);
-    menu.close();
-  });
-});
+function addScroll(scroll: boolean) {
+  document.getElementById('scroller').style.display = scroll ? 'block' : 'none';
+}
+
+const cbScroll = document.getElementById('cb-scroll') as HTMLInputElement;
+
+document
+  .getElementById('cb-scroll')
+  .addEventListener('change', () => addScroll(cbScroll.checked));
